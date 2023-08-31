4b13ef1dddb320175c1c498b0c763b2aadcf045a
Zhakshylyk Kasymbekov <jakkasymbekov@gmail.com>
2023-08-31 18:37:17 +0600

    BrokedApp task fixed after testing

--- src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/BrokenApp.java
@@ -2,6 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class BrokenApp {
     public static void main(String[] args) {
-        System.out.println("fix\nMe");
+        System.out.println("fixMe");
     }
 }
